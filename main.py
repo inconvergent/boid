737db5b6755dd2ab16b59f2fdaa5ad6f196cfcc6
anders hoff <shall.be.lifted@gmail.com>
2013-06-24 23:31:19 +0200

    .

--- main.py
+++ main.py
@@ -36,8 +36,10 @@ def main():
   ONE = 1./N
   STP = ONE
 
+  FUZZ = STP*0.1
+
   NUM = 200
-  FLOCK_RAD = 0.1
+  FLOCK_RAD = 0.01
 
   SEPARATION_PRI = 0.5
   ALIGNMENT_PRI = 0.3
@@ -71,12 +73,12 @@ def main():
 
       self.R[:] = cdist(*[ colstack((self.X,self.Y)) ]*2 ) 
 
-    def set_phi(self):
+    #def set_phi(self):
 
-      for i in xrange(NUM):
-        dx = self.X[i] - self.X
-        dy = self.Y[i] - self.Y
-        self.A[i,:] = arctan2(dy,dx)
+      #for i in xrange(NUM):
+        #dx = self.X[i] - self.X
+        #dy = self.Y[i] - self.Y
+        #self.A[i,:] = arctan2(dy,dx)
 
     def set_flock(self):
 
@@ -87,6 +89,10 @@ def main():
         self.F[i,:] = inflock[:]
 
     def iterate(self):
+
+      alpha = rand( NUM ) *pi*2.
+      self.DX[:] += cos( alpha )*FUZZ
+      self.DY[:] += sin( alpha )*FUZZ
 
       self.X[:] += self.DX[:]
       self.Y[:] += self.DY[:]
@@ -105,10 +111,15 @@ def main():
 
     def separation(self):
 
-      inflock = self.F[self.i,:]
-      if inflock.sum() > 1:
+      self.SEPX[self.i] = 0.
+      self.SEPY[self.i] = 0.
 
-        scale = 1./sqrt( self.R[self.i,inflock] )
+      inflock = self.F[self.i,:]
+      inflock[self.i] = False
+
+      if inflock.sum() > 0:
+
+        scale = 1./square( self.R[self.i,inflock] )
 
         dx = ( self.X[i] - self.X[inflock] ) * scale
         dy = ( self.Y[i] - self.Y[inflock] ) * scale
@@ -116,50 +127,55 @@ def main():
         self.SEPX[self.i] = -dx.sum()
         self.SEPY[self.i] = -dy.sum()
 
-    def alignment(self):
+    #def alignment(self):
 
-      inflock = self.F[self.i,:]
-      if inflock.sum() > 1:
+      #self.ALIX[self.i] = 0.
+      #self.ALIY[self.i] = 0.
 
-        scale = 1./self.R[self.i,inflock]**2.
+      #inflock = self.F[self.i,:]
+      #inflock[self.i] = False
 
-        dx = (self.DX[inflock]*scale).sum()
-        dy = (self.DY[inflock]*scale).sum()
+      #if inflock.sum() > 0:
 
-        self.ALIX[self.i] = dx
-        self.ALIY[self.i] = dy
+        #scale = 1./square( self.R[self.i,inflock] )
+
+        #dx = self.DX[inflock] * scale
+        #dy = self.DY[inflock] * scale
+
+        #self.ALIX[self.i] = dx.sum()
+        #self.ALIY[self.i] = dy.sum()
 
     def step(self):
 
       self.DX[self.i] = 0.
       self.DY[self.i] = 0.
 
-      acc = sqrt(square(self.SEPX[self.i]) + square(self.SEPY[self.i]))
-      if acc > MAXACC:
-        scale = MAXACC / acc
+      acc = sqrt( square( self.SEPX[self.i] ) + \
+                  square( self.SEPY[self.i] ) )
+      available = MAXACC
+      if acc > available:
+        scale = available / acc
         self.SEPX[self.i] = self.SEPX[self.i] * scale
         self.SEPY[self.i] = self.SEPY[self.i] * scale
         accsum = MAXACC
       else:
         accsum = acc
 
-      self.DX[self.i] = self.SEPX[self.i]
-      self.DY[self.i] = self.SEPY[self.i]
+      self.DX[self.i] += self.SEPX[self.i]
+      self.DY[self.i] += self.SEPY[self.i]
 
       if accsum >= MAXACC:
         return
 
-      acc = sqrt(square(self.ALIX[self.i]) + square(self.ALIY[self.i]))
-      if acc > MAXACC:
-        scale = MAXACC / acc
-        self.ALIX[self.i] = self.ALIX[self.i] * scale
-        self.ALIY[self.i] = self.ALIY[self.i] * scale
-        accsum = MAXACC
-      else:
-        accsum = acc
+      #acc = sqrt(square(self.ALIX[self.i]) + square(self.ALIY[self.i]))
+      #available = MAXACC - accsum
+      #if acc > available:
+        #scale = available / acc
+        #self.ALIX[self.i] = self.ALIX[self.i] * scale
+        #self.ALIY[self.i] = self.ALIY[self.i] * scale
 
-      self.DX[self.i] += self.ALIX[self.i]
-      self.DY[self.i] += self.ALIY[self.i]
+      #self.DX[self.i] += self.ALIX[self.i]
+      #self.DY[self.i] += self.ALIY[self.i]
 
   # BEGIN
 
@@ -168,15 +184,16 @@ def main():
   F = []
   for i in xrange(NUM):
     r = rand()*0.1
+    r = 0.1
     alpha = rand()*pi*2.
-    x = C + cos(alpha)*r
-    y = C + sin(alpha)*r
+    x = C + cos( alpha )*r
+    y = C + sin( alpha )*r
     the = rand()*pi*2.
-    dx = cos(the)*STP
-    dy = sin(the)*STP
+    dx = cos( the )*STP
+    dy = sin( the )*STP
     dx = 0
     dy = 0
-    F.append(Boid(x,y,dx,dy))
+    F.append( Boid(x,y,dx,dy) )
 
 
   plt.figure(0)
@@ -193,7 +210,6 @@ def main():
       plt.draw()
 
     M.set_dist()
-    #M.set_phi()
     M.set_flock()
 
     for boid in F:
